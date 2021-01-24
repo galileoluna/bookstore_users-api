1330dfbf632afae0280b295ed3301f6ee8ac9f34
Galileo Luna <galileoungs@gmail.com>
2021-01-24 19:35:15 -0300

    start to connect with mysql

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -4,6 +4,8 @@ package users
 import (
 	"fmt"
 
+	"github.com/galileoluna/bookstore_users-api/datasources/mysql/users_db"
+	"github.com/galileoluna/bookstore_users-api/utils/date_utils"
 	"github.com/galileoluna/bookstore_users-api/utils/errors"
 )
 
@@ -12,6 +14,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -35,6 +41,7 @@ func (user *User) Save() *errors.RestErr {
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 
 	}
+	user.DateCreated = date_utils.GetNowString()
 	usersDB[user.Id] = user
 	return nil
 }
